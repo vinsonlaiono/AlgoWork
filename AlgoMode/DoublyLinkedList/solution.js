e00fd78c7a97d719dcfba9f2039f47243a3fe757
vinson aiono <vinson.aiono@gmail.com>
2019-09-14 21:48:38 -0700

    DLL solutions

--- AlgoMode/DoublyLinkedList/solution.js
+++ AlgoMode/DoublyLinkedList/solution.js
@@ -1,7 +1,84 @@
+// Create a doubly linked list
+// addtoEnd
+// removeFromEnd
+// addToFrom
+// removeFromFront
+// contains
+class DLL {
+    constructor(){
+        this.head = null;
+    }
+}
 class Node{
-    constructor(name){
-        this.value;
-        this.prev;
-        this.next;
+    constructor(value){
+        this.value = value;
+        this.prev = null;
+        this.next = null;
+    }
+}
+DLL.prototype.addToEnd = function(value){
+    let newNode = new Node(value);
+    if(!this.head) {
+        this.head = newNode;
+        this.tail = newNode;
+        return this;
+    }
+    else {
+        let runner = this.head;
+        while(runner.next != null){
+            runner = runner.next;
+        }
+        runner.next = newNode;
+        newNode.prev = runner;
+    }
+    return this;
+}
+DLL.prototype.removeFromEnd = function(){
+    if(!this.head) return this;
+    if(!this.head.next){ this.head = null; return;}
+    else {
+        let runner = this.head;
+        while(runner.next != null){
+            runner = runner.next;
+        }
+        runner.prev.next = null;
+    }
+    return this;
+}
+DLL.prototype.addToFront = function(value){
+    let newNode = new Node(value);
+    if(!this.head){
+        this.head = newNode;
+        this.tail = newNode;
+    } else {
+        newNode = this.head;
+        this.head = newNode;
+    }
+    return this;
+}
+
+DLL.prototype.removeFromFront = function(){
+    if(!this.head){
+        return this;
+    } else if(!this.head.next) {
+        this.head = null;
+        this.tail = null;
+    }
+    return this;
+}
+
+DLL.prototype.contains = function(value){
+    if(!this.head){
+        return this;
+    } else {
+        let runner = this.head;
+        while(runner){
+            if(runner.value = value){
+                return true;
+            } else {
+                runner = runner.next;
+            }
+        }
+        return this;
     }
 }
