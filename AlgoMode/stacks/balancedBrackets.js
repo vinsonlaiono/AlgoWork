60efae3453b44bfce7a91809512fdc2d2ccb5a64
vinson aiono <vinson.aiono@gmail.com>
2019-09-25 21:45:53 -0700

    arrays solutions

--- AlgoMode/stacks/balancedBrackets.js
+++ AlgoMode/stacks/balancedBrackets.js
@@ -1,3 +1,23 @@
+
+// (({}()))
+
 function balancedBracket(str){
-    console.log(str)
+    console.log(str);
+    let brackets = {};
+    for(let i=0; i < str.length; i++){
+        if(str[i] in brackets){
+            brackets[str[i]]++;
+        } else {
+            brackets[str[i]] = 1;
+        }
+        if(str[i] === '}'){brackets['{']--;}
+        if(str[i] === ')'){brackets['(']--;}
+        if(str[i] === ']'){brackets['[']--;}
+    }
+    if(brackets['}'] === 0 || brackets['['] === 0 || brackets['('] === 0){
+        return true;
+    }
+    return false;
 }
+
+console.log(balancedBracket('(({}()))('));
