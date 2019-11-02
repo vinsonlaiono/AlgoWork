4e511022c885d8ad015f006a7ef80b4049c52caf
vinson aiono <vinson.aiono@gmail.com>
2019-11-01 18:30:31 -0700

    python bubblesort

---
+++ Python/DIP/sorting/bubblesort.py
@@ -0,0 +1,15 @@
+def bubblesort(arr):
+    isSorted = False
+    count = 0
+    while not isSorted:
+        isSorted = True
+        for i in range(len(arr) - 1 - count):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                isSorted = False
+        count += 1
+    return arr   
+         
+print([4,2,3,1,7,4,6,5])
+print(bubblesort([4,2,3,1,7,4,6,5]))
+    
